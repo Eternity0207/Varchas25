8e9b71637cafc2b7369f41228911f3ebcdcb9eb6
Nafees Ansari <nafeesansa333@gmail.com>
2025-10-16 01:40:14 +0530

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { motion, AnimatePresence } from "framer-motion";
-import Home from "./pages/Home";
-import Loader from "./components/loader";
+import Home from "./pages/home";
 import "./index.css";
 
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 5500);
-    return () => clearTimeout(timer);
-  }, []);
-
-
   return (
-    <AnimatePresence mode="wait">
-      {loading ? (
-        <motion.div
-          key="loader"
-          initial={{ y: 0, opacity: 1 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: "-100%", opacity: 0 }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          className="fixed top-0 left-0 w-full h-full z-50"
-        >
-          <Loader />
-        </motion.div>
-      ) : (
-        <motion.div
-          key="content"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
-        >
-          <Router>
-            {/* <Navbar /> */}
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </Router>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    
+    <Router>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
